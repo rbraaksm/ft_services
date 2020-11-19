3b92f05b3d017eef753bbf852d7063860977db07
rbraaksm <renebraaksma@hotmail.com>
2020-11-19 16:19:06 +0100

    phpmyadmin

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-#!bin/zsh
+# !bin/zsh
+rm -rf ~/.minikube/machines
+mkdir -p ~/goinfre/machines
+ln -s ~/goinfre/machines ~/.minikube/machines
 
 echo "\n----> Setting Minikube environment <----\n"
 minikube delete
@@ -12,6 +15,19 @@ echo "\n----> Creating metalLB environment <----\n"
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb.yaml
 
+# Need to setup serviceaccount metallb?
+kubectl apply -f srcs/serviceaccount.yaml
+
 eval $(minikube docker-env)
 
+Creating an serviceaccount:
+kubectl apply -f srcs/serviceaccount.yaml
+
+# Creating nginx:
+docker build -t my-nginx srcs/nginx/
+kubectl apply -f srcs/nginx/nginx.yaml
+
+# Creating phpmyadmin:
+docker build -t my-phpmyadmin srcs/phpmyadmin/
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 
