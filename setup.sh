8aea359a6a122cb73db9202660d04686a6d24789
Rene Braaksma <rbraaksm@f0r1s14.codam.nl>
2020-11-25 09:02:46 +0100

    new

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #!bin/zsh
+# create link in goinfre to save memory
+rm -rf ~/.minikube/machines
+mkdir -p ~/goinfre/machines
+ln -s ~/goinfre/machines ~/.minikube/machines
 
-echo "\n----> Setting Minikube environment <----\n"
-minikube delete
+# echo "\n----> Setting Minikube environment <----\n"
+# minikube delete
 minikube start --driver=virtualbox
-minikube addons enable metrics-server
-minikube addons enable metallb
-minikube addons enable dashboard
+# minikube addons enable metrics-server
+# minikube addons enable metallb
+# minikube addons enable dashboard
 
 
-echo "\n----> Creating metalLB environment <----\n"
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f srcs/metallb.yaml
+# echo "\n----> Creating metalLB environment <----\n"
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# kubectl apply -f srcs/metallb.yaml
 
-eval $(minikube docker-env)
+# eval $(minikube docker-env)
 
 
