bf65e538dc79145b7acb47b29b795a46450d27ad
rbraaksm <renebraaksma@hotmail.com>
2020-12-01 10:40:11 +0100

    wordpress

--- setup.sh
+++ setup.sh
@@ -5,75 +5,74 @@
 # # mkdir -p ~/goinfre/machines
 # # ln -s ~/goinfre/machines ~/.minikube/machines
 
-# echo "\n----> Setting Minikube environment <----\n"
-# minikube stop
-# minikube delete
-# minikube start --vm-driver=virtualbox
-# # minikube addons enable metrics-server
-# minikube addons enable metallb
-# minikube addons enable dashboard
-
-# echo "\n----> Creating metalLB environment <----\n"
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# kubectl apply -f srcs/metallb.yaml
-
-# eval $(minikube docker-env)
-
-# # Creating an serviceaccount:
-# kubectl create serviceaccount remco
-# kubectl apply -f srcs/serviceaccount.yaml
-
-# # Creating nginx:
-# # docker build -t my-nginx srcs/nginx/
-# # kubectl apply -f srcs/nginx/nginx.yaml
-
-# # # Creating ftps:
-# # docker build -t my-ftps srcs/ftps/
-# # kubectl apply -f srcs/ftps/ftps.yaml
-
-# # Creating mysql:
-# docker build -t my_mysql srcs/mysql/
-# kubectl apply -f srcs/mysql/mysql.yaml
-
-# # # Creating phpmyadmin:
-# # docker build -t my-phpmyadmin srcs/phpmyadmin/
-# # kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
-
-# # # Creating wordpress:
-# # docker build -t wordpress srcs/wordpress/
-# # kubectl apply -f srcs/wordpress/wordpress.yaml
-
-
-
-#!/bin/zsh
+echo "\n----> Setting Minikube environment <----\n"
+minikube stop
 minikube delete
 minikube start --vm-driver=virtualbox
 minikube addons enable metallb
 minikube addons enable dashboard
+minikube addons enable metrics-server
 
-# Configuring metallb:
-
+echo "\n----> Creating metalLB environment <----\n"
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb.yaml
 
-# Setting up Minikube's Docker:
 eval $(minikube docker-env)
 
-# # Creating an serviceaccount:
-# kubectl create serviceaccount remco
+# Creating an serviceaccount:
 kubectl apply -f srcs/serviceaccount.yaml
 
 # Creating nginx:
+docker build -t my-nginx srcs/nginx/
+kubectl apply -f srcs/nginx/nginx.yaml
 
-# docker build -t my_nginx srcs/nginx/
-# kubectl apply -f srcs/nginx.yaml
+# # Creating ftps:
+docker build -t my-ftps srcs/ftps/
+kubectl apply -f srcs/ftps/ftps.yaml
 
-# Creating ftps:
-
-# docker build -t my_ftps srcs/ftps/
-# kubectl apply -f srcs/ftps.yaml
-
-# Creating mysql:
-
-docker build -t my_mysql srcs/mysql/
+# # Creating mysql:
+docker build -t my-mysql srcs/mysql/
 kubectl apply -f srcs/mysql/mysql.yaml
+
+# Creating phpmyadmin:
+docker build -t my-phpmyadmin srcs/phpmyadmin/
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+
+# Creating wordpress:
+docker build -t my-wordpress srcs/wordpress/
+kubectl apply -f srcs/wordpress/wordpress.yaml
+
+
+
+# #!/bin/zsh
+# minikube delete
+# minikube start --vm-driver=virtualbox
+# minikube addons enable metallb
+# minikube addons enable dashboard
+
+# # Configuring metallb:
+
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# kubectl apply -f srcs/metallb.yaml
+
+# # Setting up Minikube's Docker:
+# eval $(minikube docker-env)
+
+# # # Creating an serviceaccount:
+# # kubectl create serviceaccount remco
+# kubectl apply -f srcs/serviceaccount.yaml
+
+# # Creating nginx:
+
+# # docker build -t my_nginx srcs/nginx/
+# # kubectl apply -f srcs/nginx.yaml
+
+# # Creating ftps:
+
+# # docker build -t my_ftps srcs/ftps/
+# # kubectl apply -f srcs/ftps.yaml
+
+# # Creating mysql:
+
+# docker build -t my-mysql srcs/mysql/
+# kubectl apply -f srcs/mysql/mysql.yaml
--- srcs/phpmyadmin/setup.sh
+++ srcs/phpmyadmin/setup.sh
@@ -1,2 +1,12 @@
-php7-fpm
+php-fpm7
 nginx
+
+while true; do
+	sleep 10
+	ps | grep nginx | grep master
+	if [ $? == 1 ]; then break
+	fi
+	ps | grep php-fpm | grep master
+	if [ $? == 1 ]; then break
+	fi
+done
