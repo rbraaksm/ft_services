f0065245d8af33adbfbe4df1a0f153e1387f1f54
rbraaksm <renebraaksma@hotmail.com>
2020-12-24 14:55:26 +0100

    intra

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@
 # # mkdir -p ~/goinfre/machines
 # # ln -s ~/goinfre/machines ~/.minikube/machines
 
-# minikube stop
 minikube delete
 minikube start --vm-driver=virtualbox
 minikube addons enable metallb
@@ -13,41 +12,38 @@ minikube addons enable dashboard
 minikube addons enable metrics-server
 
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f srcs/metallb.yaml
+kubectl apply -f srcs/service-metallb/metallb.yaml
 
 eval $(minikube docker-env)
 
-kubectl apply -f srcs/serviceaccount.yaml
+kubectl apply -f srcs/service-metallb/serviceaccount.yaml
 
-# docker build -t my-nginx srcs/nginx/
-# kubectl apply -f srcs/nginx/nginx.yaml
+docker build -t container-nginx srcs/nginx/
+kubectl apply -f srcs/nginx/nginx.yaml
 
-docker build -t my-ftps srcs/ftps/
+docker build -t container-ftps srcs/ftps/
 kubectl apply -f srcs/ftps/ftps.yaml
 
-# docker build -t my-ftps srcs/ftps_mine/
-# kubectl apply -f srcs/ftps_mine/ftps.yaml
+docker build -t container-mysql srcs/mysql/
+kubectl apply -f srcs/mysql/mysql.yaml
 
-# docker build -t my-ftps srcs/ftps_tim/
-# kubectl apply -f srcs/ftps_tim/ftps.yaml
+docker build -t container-phpmyadmin srcs/phpmyadmin/
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 
-# docker build -t my-mysql srcs/mysql/
-# kubectl apply -f srcs/mysql/mysql.yaml
+docker build -t container-wp srcs/wordpress/
+kubectl apply -f srcs/wordpress/wordpress.yaml
 
-# docker build -t my-phpmyadmin srcs/phpmyadmin/
-# kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
+docker build -t container-grafana srcs/grafana/
+kubectl apply -f srcs/grafana/grafana.yaml
 
-# docker build -t my-wordpress srcs/wordpress/
-# kubectl apply -f srcs/wordpress/wordpress.yaml
+docker build -t container-influxdb srcs/influxdb/
+kubectl apply -f srcs/influxdb/influxdb.yaml
 
-# docker build -t my-grafana srcs/grafana/
-# kubectl apply -f srcs/grafana/grafana.yaml
-
-# docker build -t my-influxdb srcs/influxdb/
-# kubectl apply -f srcs/influxdb/influxdb.yaml
-
-# docker build -t my-telegraf srcs/telegraf/
-# kubectl apply -f srcs/telegraf/telegraf.yaml
+docker build -t container-telegraf srcs/telegraf/
+kubectl apply -f srcs/telegraf/telegraf.yaml
 
 # Log ins:
-# grafana rene:ft_services
+# wordpress		rbraaksm:ft_services
+# phpmyadmin	mysql:ft_services
+# FTPS			rbraaksm:ft_services
+# grafana 		admin:admin
--- srcs/grafana/setup.sh
+++
@@ -1,6 +0,0 @@
-cd grafana-7.3.4
-./bin/grafana-server
-
-# grafana-cli admin reset-admin-password ft_services
-ENV		GF_SECURITY_ADMIN_USER=rbraaksm
-ENV		GF_SECURITY_ADMIN_PASSWORD=ft_services
